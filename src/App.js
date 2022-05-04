7c98f0a4af1c1d5fe9d12696cc68a5f43e91464e
mikeydgithub <ampgzo2@gmail.com>
2022-05-04 13:06:50 -0400

    gallery component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-unused-vars */
 // import react in every file component. also import images or css at the top.
 import React from 'react';
 import './App.css';
 import About from './components/About';
 import Nav from './components/Nav';
+import Gallery from './components/Gallery';
 
 
 // language called JSX that can represnet HTML in JS.
@@ -11,6 +13,7 @@ function App() {
    <div>
      <Nav></Nav>
      <main>
+       <Gallery></Gallery>
        <About></About>
      </main>
    </div>
--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,11 +1,25 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import {capitalizeFirstLetter} from '../../utils/helpers';
+import photo from "../../assets/small/commercial/0.jpg"
 
-function Gallery() {
+// create the gallery component
+function Gallery(props) {
+    const currentCategory = {
+        name: "commercial",
+        description: "Photos of grocery stores, food trucks, and other commercial projects."
+    };
     return (
         <section>
-            <h1>NAME</h1>
-            <p>DESCRIPTION</p>
+            <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
+            <p>{currentCategory.name}</p>
+            <div>
+            <img 
+            src={photo}
+            alt="Commercial Exmaple"
+            className="img-thumbnail mx-1"
+            />
+            </div>
         </section>
     );
 }
