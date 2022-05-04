94d9a2864c5b50bfd23c3a73ea6f607f73bd4d1e
mikeydgithub <ampgzo2@gmail.com>
2022-05-04 12:53:12 -0400

    create gallery compnent and index

---
+++ src/components/Gallery/index.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+function Gallery() {
+    return (
+        <section>
+            <h1>NAME</h1>
+            <p>DESCRIPTION</p>
+        </section>
+    );
+}
+
+export default Gallery;
